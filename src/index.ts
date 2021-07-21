d6b83ed74837858f60ae509289b34be95a1e3502
Pavel Grivachev <pavel@fivetran.com>
2021-07-21 20:45:29 +0300

    export TypeFactory

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './SimpleCatalog';
 export * from './SimpleColumn';
 export * from './SimpleTable';
 export * from './SimpleType';
+export * from './TypeFactory';
 export * from './types/zetasql/local_service/AnalyzeRequest';
 export * from './types/zetasql/TypeKind';
