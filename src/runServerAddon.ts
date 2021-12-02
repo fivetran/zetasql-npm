c68412488f5871b19b653949baa9a82139a4cb12
Dmitry Boykov <dmitry.boykov@fivetran.com>
2021-12-02 11:16:56 +0300

    Add comment into function instead of eslint hint hiding

--- src/runServerAddon.ts
+++ src/runServerAddon.ts
@@ -4,5 +4,6 @@ const zetaSQLServer = ffi.Library(__dirname + '/zetasql/libremote_server', {
   RunServer: ['void', []],
 });
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-zetaSQLServer.RunServer.async(function (err, res) {});
+zetaSQLServer.RunServer.async(function (err, res) {
+  // do nothing
+});
