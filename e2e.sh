a6c99aeb38bcef010df2b1c857f2052e1b01ae48
Pavel Grivachev <pavel@fivetran.com>
2022-03-13 12:57:28 +0300

    refactor e2e test (#25)

--- e2e.sh
+++ e2e.sh
@@ -9,11 +9,11 @@ Terminating..."
 
 actualResult=$(node lib/TestZetaSqlServer.js)
 if [[ "${expectedResult}" == "${actualResult}" ]]; then
-    printf "e2e test passed successfully!"
+    printf "e2e test passed successfully!\n"
     exit 0;
 fi
 
-printf "e2e test error!"
+printf "e2e test error!\n"
 printf "Expected result is:\n ${expectedResult}\n\n"
 printf "Actual result is:\n ${actualResult}\n\n"
 exit 1;
