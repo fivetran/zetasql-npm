5c3fd92a8c6c414fb3b1988f7fb2725b047aac7d
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-15 13:31:09 +0300

    Enable both versioned features and all available

--- src/LanguageOptions.ts
+++ src/LanguageOptions.ts
@@ -3,10 +3,11 @@ import {
   LanguageOptionsProto,
   LanguageOptionsProto__Output,
 } from './types/zetasql/LanguageOptionsProto';
+import { LanguageVersion } from './types/zetasql/LanguageVersion';
 import { ZetaSQLClient } from './ZetaSQLClient';
 
 export class LanguageOptions {
-  static maxFeatures: LanguageOptionsProto__Output | undefined;
+  private static maxFeatures: LanguageOptionsProto__Output | undefined;
 
   options: LanguageOptionsProto = {};
 
@@ -20,15 +21,32 @@ export class LanguageOptions {
     return LanguageOptions.maxFeatures;
   }
 
+  async setLanguageVersion(version: LanguageVersion): Promise<void> {
+    this.setEnabledLanguageFeatures(await LanguageOptions.getLanguageFeaturesForVersion(version));
+  }
+
+  static async getLanguageFeaturesForVersion(
+    languageVersion: LanguageVersion,
+  ): Promise<Set<LanguageFeature>> {
+    const features = new Set<LanguageFeature>();
+    (
+      await ZetaSQLClient.getInstance().getLanguageOptions({ languageVersion })
+    )?.enabledLanguageFeatures.forEach(f => features.add(f));
+
+    return features;
+  }
+
   async enableMaximumLanguageFeatures(): Promise<LanguageOptions> {
     const features = new Set<LanguageFeature>();
-    (await LanguageOptions.getMaxFeatures())?.enabledLanguageFeatures.forEach(
-      (f: LanguageFeature) => {
-        features.add(f);
-      },
-    );
+    (await LanguageOptions.getMaxFeatures())?.enabledLanguageFeatures.forEach(f => features.add(f));
     this.setEnabledLanguageFeatures(features);
     return this;
+  }
+
+  enableLanguageFeature(feature: LanguageFeature): void {
+    if (!this.options.enabledLanguageFeatures?.includes(feature)) {
+      this.options.enabledLanguageFeatures?.push(feature);
+    }
   }
 
   setEnabledLanguageFeatures(enabledLanguageFeatures: Set<LanguageFeature>): void {
