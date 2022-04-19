7fef64fab42a4616bdcde6e0d4c6fd9cb59bf09e
Pavel Grivachev <pavel@fivetran.com>
2022-04-19 22:05:25 +0300

    add e2e tests

--- src/test/TestZetaSqlServer.ts
+++ src/test/TestZetaSqlServer.ts
@@ -1,9 +1,9 @@
-import { runServer, terminateServer, ZetaSQLClient } from '.';
+import { runServer, terminateServer, ZetaSQLClient } from '..';
 
 // This test should run and terminate ZetaSQL gRPC server
 
 const port = 50005;
-void new Promise(resolve => setTimeout(resolve, 3000))
+new Promise(resolve => setTimeout(resolve, 3000))
   .then(() => {
     console.log('Init...');
     ZetaSQLClient.init(port);
@@ -19,6 +19,7 @@ void new Promise(resolve => setTimeout(resolve, 3000))
   })
   .then(() => {
     console.log('Terminated');
-  });
+  })
+  .catch(e => console.error(e));
 
-runServer(port).catch(err => console.error(err));
+runServer(port).catch(e => console.error(e));
