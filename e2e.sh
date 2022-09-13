6214d25ee6c7c2a7fae5e5a52903a3dbc565fa77
Pavel Grivachev <pavel@fivetran.com>
2022-09-13 19:11:27 +0200

    add test

--- e2e.sh
+++ e2e.sh
@@ -28,7 +28,7 @@ if [[ "${expectedResult}" != "${actualResult}" ]]; then
     exit 1;
 fi
 
-# TODO: fix this test on mac
+# TODO: fix this test on macOS
 # expectedResult="Running server on 0.0.0.0:50005...
 # Server listening on 0.0.0.0:50005
 # Tests passed"
@@ -40,6 +40,19 @@ fi
 #     printf "Actual result is:\n ${actualResult}\n\n"
 #     exit 1;
 # fi
+
+# TODO: fix this test on macOS
+expectedResult="Running server on 0.0.0.0:50005...
+Server listening on 0.0.0.0:50005
+Tests passed"
+actualResult=$(node lib/test/ManyAdditionOperations.js)
+
+if [[ "${expectedResult}" != "${actualResult}" ]]; then
+    printf "TestQuery error!\n"
+    printf "Expected result is:\n ${expectedResult}\n\n"
+    printf "Actual result is:\n ${actualResult}\n\n"
+    exit 1;
+fi
 
 printf "e2e tests passed successfully!\n"
 exit 0;
