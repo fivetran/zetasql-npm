4d49719b3c47404ef04720cc76df88d7ac0b247b
Pavel Grivachev <pavel@fivetran.com>
2023-03-25 16:05:24 +0100

    add linux arm binary

--- prepare_linux_arm_64_archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ../zetasql/out/linux/arm64/libremote_server.so libremote_server_arm.so
-tar -zcf zetasql/linux-arm-64/libremote_server_arm.so.tar.gz libremote_server_arm.so
-rm libremote_server_arm.so
