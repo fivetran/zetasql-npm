8b28e09e00eb66b88eb08bebbe99d79a9e5d8434
Pavel Grivachev <pavel@fivetran.com>
2023-03-25 18:26:25 +0100

    rename

--- snowflake/prepare_darwin_arm_64_archive.sh
+++ snowflake/prepare_darwin_arm_64_archive.sh
@@ -2,5 +2,5 @@
 
 cp ../../zetasql-snowflake/bazel-bin/zetasql/local_service/libremote_server.dylib libremote_server_arm.dylib
 install_name_tool -id "@loader_path/libremote_server_arm.dylib" libremote_server_arm.dylib
-tar -zcf snowflake-bin/darwin-arm-64/libremote_server_arm.dylib.tar.gz libremote_server_arm.dylib
+tar -zcf bin/darwin-arm-64/libremote_server_arm.dylib.tar.gz libremote_server_arm.dylib
 rm libremote_server_arm.dylib
