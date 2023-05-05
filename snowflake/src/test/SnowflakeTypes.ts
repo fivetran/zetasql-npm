453b55a3a1218c662cd5e2f43283ba3d03b28890
Pavel Grivachev <pavel@fivetran.com>
2023-05-05 17:21:35 +0200

    fix lint

--- snowflake/src/test/SnowflakeTypes.ts
+++ snowflake/src/test/SnowflakeTypes.ts
@@ -41,7 +41,7 @@ async function runTest(): Promise<void> {
     console.log('Tests failed');
     console.error(e);
   }
-  await terminateServer();
+  terminateServer();
 }
 
 async function analyze(sqlStatement: string): Promise<AnalyzeResponse__Output> {
