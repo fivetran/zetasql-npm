f7b1e8cd576fd3774ae7a5e33406d7720dcb3a3d
Pavel Grivachev <pavel@fivetran.com>
2023-05-06 22:54:37 +0200

    fix server run

--- zetasql/src/server.ts
+++ zetasql/src/server.ts
@@ -1,4 +1,4 @@
-import { ChildProcess, exec } from 'child_process';
+import { ChildProcess, execFile } from 'child_process';
 
 let childProcess: ChildProcess;
 
@@ -7,12 +7,12 @@ export function runServer(port: number): Promise<void> {
     (process.arch.startsWith('arm') ? '_arm' : '') + (process.platform === 'darwin' ? '' : '.so');
 
   return new Promise<void>(() => {
-    childProcess = exec(`${__dirname}/zetasql/remote_server${extension} ${port}`);
+    childProcess = execFile(`${__dirname}/zetasql/remote_server${extension}`, [port.toString()]);
     childProcess.stdout?.on('data', data => process.stdout.write(data));
     childProcess.stderr?.on('data', data => process.stdout.write(data));
   });
 }
 
 export function terminateServer(): void {
-  childProcess.kill();
+  childProcess.kill('SIGKILL');
 }
