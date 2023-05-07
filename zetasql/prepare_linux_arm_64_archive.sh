cbe4eebb2b837888fb59b2fbfad11e3595e1261b
Pavel Grivachev <pavel@fivetran.com>
2023-05-07 10:29:49 +0200

    add linux arm archive

--- zetasql/prepare_linux_arm_64_archive.sh
+++ zetasql/prepare_linux_arm_64_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ../../zetasql/out/linux/arm64/libremote_server.so libremote_server_arm.so
-tar -zcf bin/linux-arm-64/libremote_server_arm.so.tar.gz libremote_server_arm.so
-rm libremote_server_arm.so
+cp ../../zetasql/out/linux/arm64/remote_server_executable remote_server_arm.so
+tar -zcf bin/linux-arm-64/remote_server_arm.so.tar.gz remote_server_arm.so
+rm remote_server_arm.so
