68406ad3205e898aa1e0ca8393a1129570e6b8c0
Pavel Grivachev <pavel@fivetran.com>
2023-05-09 21:41:34 +0200

    Update libs (#64)
    * try another macos build
    
    * update artifacts [skip ci]
    
    * refactor
    
    * add more tests
    
    * add linux arm library
    
    * enable linux arm tests
    
    * enable ci for snowflake
    
    * use artifacts from master
    
    * update artifacts [skip ci]
    
    * update linux arm executable
    
    * bump versions
    
    ---------
    
    Co-authored-by: pgrivachev <pgrivachev@users.noreply.github.com>

--- snowflake/prepare_linux_arm_64_archive.sh
+++ snowflake/prepare_linux_arm_64_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ../../zetasql/out/linux/arm64/remote_server_executable remote_server_arm.so
+cp ../../zetasql-snowflake/out/linux/arm64/remote_server_executable remote_server_arm.so
 tar -zcf bin/linux-arm-64/remote_server_arm.so.tar.gz remote_server_arm.so
 rm remote_server_arm.so
