78ad857529c2fc14840ddbce5d358da0ed27d180
Pavel Grivachev <pavel@fivetran.com>
2023-08-10 18:42:15 +0200

    add test

--- snowflake/src/test/SnowflakeSpecific.ts
+++ snowflake/src/test/SnowflakeSpecific.ts
@@ -23,14 +23,42 @@ async function runTest(): Promise<void> {
   try {
     await analyze(
       `select *,
-       array_construct(1),
+       array_construct(1) a,
        's'::varchar,
-       try_cast('s' as int64),
+       try_cast('s' as int),
        as_boolean(true),
        try_parse_json('{}'), parse_json('{"a":"b"}'):a,
        -'1', +'1', 1 + '5', 3 % 2, null / 2,
        pi(), truncate(12.2, 2), datediff(year, '2021-01-01', '2021-02-28'),
        from table_a as A inner join lateral (select * from table_b as B);`,
+    );
+    await analyze(
+      `with sales as (
+          select 'north' as region, 'apple' as product, 100 as revenue union all
+          select 'south' as region, 'banana' as product, 30 as revenue
+      )
+      select 
+          region,
+          product,
+          sum(revenue) as total_revenue
+      from 
+          sales
+      group by grouping sets (
+          (region), 
+          (product), 
+          ()
+      );`,
+    );
+    await analyze(
+      `with sales_data as (
+          select 'electronics' as category, 2021 as year, 1 as quarter, 1000 as sales
+          union all
+          select 'electronics', 2021, 2, 1200
+      )
+      select category, year, quarter, sum(sales) as total_sales
+      from sales_data
+      group by rollup (category, year, quarter)
+      order by category, year, quarter;`,
     );
     console.log('Tests passed');
   } catch (e) {
